6e63710f3f262df83c210f79253531a692eadfe1
Madhumitha-sist <157389930+Madhumitha-sist@users.noreply.github.com>
2024-02-06 18:49:00 +0530

    Update question.c

--- 25-01-2024 /question.c
+++ 25-01-2024 /question.c
@@ -1,12 +1,22 @@
-- 👋 Hi, I’m @Madhumitha-sist
-- 👀 I’m interested in ...
-- 🌱 I’m currently learning ...
-- 💞️ I’m looking to collaborate on ...
-- 📫 How to reach me ...
-- 😄 Pronouns: ...
-- ⚡ Fun fact: ...
+#include <stdio.h>
 
-<!---
-Madhumitha-sist/Madhumitha-sist is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
-You can click the Preview link to take a look at your changes.
---->
+void findpoint(int px, int py, int qx, int qy, int *rx, int *ry) {
+    *rx = 2 * qx - px;
+    *ry = 2 * qy - py;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        int px, py, qx, qy;
+        scanf("%d %d %d %d", &px, &py, &qx, &qy);
+
+        int rx, ry;
+        findpoint(px, py, qx, qy, &rx, &ry);
+        printf("%d %d\n", rx, ry);
+    }
+
+    return 0;
+}
